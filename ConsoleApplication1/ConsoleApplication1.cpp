5210cca9fc66138e7bb31d167ec8e1386c8d4902
p0maks <golosova_v19@mail.ru>
2022-07-12 22:13:04 +0300

    second commit

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -17,5 +17,42 @@ int main()
     int** list = new int* [count + 2];
     int* cost_list = new int[count + 1];
     int* weight_list = new int[count + 1];
+    for (int i = 0; i < count + 2; i++) list[i] = new int[max_weight + 2];
+    for (int i = 0; i < count + 2; i++)
+    {
+        for (int j = 0; j < max_weight + 2; j++)
+        {
+            list[i][j] = 0;
+        }
+    }
+    for (int i = 2; i < max_weight + 2; i++)
+    {
+        list[0][i] = i - 1;
+    }
+    for (int i = 2; i < count + 2; i++)
+    {
+        int x;
+        std::cout << "Input " << i - 1 << " weight ";
+        std::cin >> x;
+        weight_list[i] = x;
+        list[i][0] = i - 1;
+        std::cout << "Input " << i - 1 << " cost ";
+        std::cin >> x;
+        cost_list[i] = x;
+    }
+    for (int i = 2; i < count + 2; i++)
+    {
+        for (int j = 2; j < max_weight + 2; j++)
+        {
+            if (j - 1 < weight_list[i])
+            {
+                list[i][j] = list[i - 1][j];
+            }
+            else if (j - 1 >= weight_list[i])
+            {
+                list[i][j] = max(list[i - 1][j], list[i - 1][j - weight_list[i]] + cost_list[i]);
+            }
+        }
 
+    }
 }
